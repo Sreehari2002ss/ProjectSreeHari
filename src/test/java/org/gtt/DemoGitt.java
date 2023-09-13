716a46af3db45b09afedb6391045d815dea3d89d
Sreehari2002ss <sreeharirox2002@gmail.com>
2023-09-13 07:33:03 +0530

    Project Structure

---
+++ src/test/java/org/gtt/DemoGitt.java
@@ -0,0 +1,25 @@
+package org.gtt;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DemoGitt {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
